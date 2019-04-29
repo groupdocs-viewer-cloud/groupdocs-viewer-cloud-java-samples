9b0334a18b58d3e3f03d3f3240a04c0e1691c054
Muhammad Rizwan Niazi <muhammad.rizwan@groupdocs.com>
2019-04-29 18:53:32 +0500

    Updated With 19.4 Release

---
+++ Examples/src/main/java/examples/Working_With_View/Viewer_Java_Create_View_With_Responsive_HTML.java
@@ -0,0 +1,40 @@
+package examples.Working_With_View;
+
+import com.groupdocs.cloud.viewer.api.*;
+import com.groupdocs.cloud.viewer.client.ApiException;
+import com.groupdocs.cloud.viewer.model.*;
+import com.groupdocs.cloud.viewer.model.requests.*;
+import examples.Utils;
+
+public class Viewer_Java_Create_View_With_Responsive_HTML {
+
+	public static void main(String[] args) {
+
+		ViewerApi apiInstance = new ViewerApi(Utils.AppSID, Utils.AppKey);
+		try {
+
+			ViewOptions viewOptions = new ViewOptions();
+
+			FileInfo fileInfo = new FileInfo();
+			fileInfo.setFilePath("viewerdocs\\three-layouts.dwf");
+			fileInfo.setPassword("");
+			fileInfo.setStorageName(Utils.MYStorage);
+
+			viewOptions.setFileInfo(fileInfo);
+
+			HtmlOptions renderOptions = new HtmlOptions();
+			renderOptions.setIsResponsive(true);
+
+			viewOptions.setRenderOptions(renderOptions);
+
+			CreateViewRequest request = new CreateViewRequest(viewOptions);
+
+			ViewResult response = apiInstance.createView(request);
+			System.out.println("Expected response type is ViewResult: " + response.getPages().size());
+
+		} catch (ApiException e) {
+			System.err.println("Exception while calling ViewerApi:");
+			e.printStackTrace();
+		}
+	}
+}
